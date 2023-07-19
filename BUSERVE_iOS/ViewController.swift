847ba63e2f0176cd80fb8df49215cad7ecfa614f
JEONG-J <ikejhc159@gmail.com>
2023-07-19 13:29:34 +0900

    Create_File

---
+++ BUSERVE_iOS/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BUSERVE_iOS
+//
+//  Created by 정의찬 on 2023/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
