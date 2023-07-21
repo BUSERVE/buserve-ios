61941c88c1cb0e2dcc730590efeb3d84f5ce54f8
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2023-07-21 17:45:01 +0900

    Color Extension

---
+++ BUSERVE_iOS/Extension/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  BUSERVE_iOS
+//
+//  Created by 정태우 on 2023/07/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let MainColor = UIColor(named: "MainColor")
+    static let SubColor = UIColor(named: "SubColor")
+    static let Quaternary = UIColor(named: "Quaternary")
+    static let Tertiary = UIColor(named: "Tertiary")
+    static let Body = UIColor(named: "Body")
+    static let Secondary = UIColor(named: "Secondary")
+    static let Background = UIColor(named: "Background")
+    static let SecondaryBackground = UIColor(named: "SecondaryBackground")
+}
