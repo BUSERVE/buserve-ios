d50191d1ef51144a8d5fa5e35ef6924bb4f499d4
Genesis <dmsgktn1685@naver.com>
2023-08-01 15:34:19 +0900

    [FEAT] BookmarkViewController 구현
    BookmarkViewController 와 BusDataTableViewCell 을 구현하였습니다
    
    Resolve: #6

--- BUSERVE_iOS/Extension/ColorExtension.swift
+++ BUSERVE_iOS/Extension/ColorExtension.swift
@@ -10,12 +10,12 @@ import UIKit
 
 extension UIColor {
     static let MainColor = UIColor(named: "MainColor") ?? UIColor(red: 18/255, green: 104/255, blue: 251/255, alpha: 1)
-    static let SubColor = UIColor(named: "SubColor")
+    static let SubColor = UIColor(named: "SubColor") ?? UIColor(red: 255/255, green: 184/255, blue: 0/255, alpha: 1)
     static let Quaternary = UIColor(named: "Quaternary")
-    static let Tertiary = UIColor(named: "Tertiary")
-    static let Body = UIColor(named: "Body")
-    static let Secondary = UIColor(named: "Secondary")
-    static let Background = UIColor(named: "Background")
+    static let Tertiary = UIColor(named: "Tertiary") ?? UIColor(red: 206/255, green: 211/255, blue: 216/255, alpha: 1)
+    static let Body = UIColor(named: "Body") ?? UIColor(red: 53/255, green: 58/255, blue: 63/255, alpha: 1)
+    static let Secondary = UIColor(named: "Secondary") ?? UIColor(red: 135/255, green: 142/255, blue: 149/255, alpha: 1)
+    static let Background = UIColor(named: "Background") ?? UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
     static let SecondaryBackground = UIColor(named: "SecondaryBackground")
     
     // LoginView Color
